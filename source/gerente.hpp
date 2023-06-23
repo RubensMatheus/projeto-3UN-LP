f3afcecbf3c6efd15179dd9f1c5c7d504029f06e
Wisla <wisla@Andres-MacBook-Pro.local>
2023-06-23 17:09:55 -0300

    Comentários doxygen em pessoa.hpp e gerente.hpp

--- source/gerente.hpp
+++ source/gerente.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file Gerente.hpp
+ * @brief Arquivo de cabeçalho para a classe Gerente
+ */
+
 #ifndef GERENTE_HPP
 #define GERENTE_HPP
 
@@ -5,25 +10,82 @@
 #include "pessoa.hpp"
 #include "funcionario.hpp"
 
+/**
+ * @class Gerente
+ * @brief Classe para representar um Gerente
+ * 
+ * @details Essa classe é definida a partir das classes bases Pessoa e Funcionario e representa um gerente na empresa. 
+ * Um gerente tem, além das propriedades de Pessoa e Funcionario, uma participação nos lucros da empresa.
+ */
 class Gerente : public Pessoa, public Funcionario {
 private:
-  float participacaoLucros;
+  float participacaoLucros; ///< Participação do Gerente nos lucros da empresa.
 
 public:
-  // Construtor vazio
+  /**
+   * @brief Construtor padrão
+   * 
+   * @details Construtor que inicializa um gerente com atributos vazios.
+   */
   Gerente() {}
 
-  // Construtor parametrizado
+  /**
+   * @brief Construtor parametrizado
+   * 
+   * @details Construtor que inicializa um gerente com valores para cada atributo.
+   * 
+   * @param nome Nome do gerente.
+   * @param cpf CPF do gerente.
+   * @param dataNascimento Data de nascimento do gerente.
+   * @param enderecoPessoal Endereço residencial do gerente.
+   * @param estadoCivil Estado civil do gerente.
+   * @param qtdFilhos Número de filhos do gerente.
+   * @param salario Salário do gerente.
+   * @param matricula Matrícula do gerente na empresa.
+   * @param ingressoEmpresa Data de ingresso do gerente na empresa.
+   * @param diasFaltas Número de dias de falta do gerente.
+   * @param participacaoLucros Participação nos lucros da empresa do gerente.
+   */
   Gerente(string nome, string cpf, Data dataNascimento, Endereco enderecoPessoal,
           string estadoCivil, int qtdFilhos, string salario, string matricula,
           Data ingressoEmpresa, int diasFaltas, float participacaoLucros);
 
-  // Getters e setters
+   /**
+   * @brief Getter para participacaoLucros
+   * 
+   * @details Permite o acesso ao valor que corresponde a participação nos lucros do gerente. 
+   * O método é const e não pode modificar atributos da classe.
+   * 
+   * @return float O valor relacionado a participação do gerente nos lucros.
+   */
   float getParticipacaoLucros() const;
+
+  /**
+   * @brief Setter para participacaoLucros
+   * 
+   * @details Determina a participação nos lucros do gerente.
+   * 
+   * @param participacaoLucros Valor a ser atribuido a participação nos lucros do gerente.
+   */
   void setParticipacaoLucros(float participacaoLucros);
 
-  // Implementação dos métodos abstratos
+  /**
+   * @brief Calcula o salário do gerente.
+   * 
+   * @details Implementação do método abstrato de Funcionario para calcular o salário do gerente.
+   * 
+   * @return float O valor calculado do salário do gerente.
+   */
   float calcularSalario() override;
+
+  /**
+   * @brief Calcula a rescisão do gerente em caso de desligamento.
+   * 
+   * @details Implementação do método abstrato de Funcionario para calcular a rescisão do gerente.
+   * 
+   * @param desligamento Data de desligamento do gerente.
+   * @return float O valor calculado da rescisão do gerente.
+   */
   float calcularRecisao(Data desligamento) override;
 };
 
--- source/pessoa.hpp
+++ source/pessoa.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file Pessoa.hpp
+ * @brief Arquivo de cabeçalho para a classe Pessoa
+ */
+
 #ifndef PESSOA_H
 #define PESSOA_H
 
@@ -5,37 +10,160 @@
 #include "util.hpp"
 using namespace std;
 
+/**
+ * @class Pessoa
+ * @brief Classe para representar uma Pessoa
+ * 
+ * @details Esta classe representa uma pessoa na empresa e seus atributos.
+ */
 class Pessoa
 {
 private:
-    string nome;
-    string cpf;
-    Data dataNascimento;
-    Endereco enderecoPessoal;
-    string estadoCivil;
-    int qtdFilhos;
+    string nome; ///< Nome da pessoa.
+    string cpf;  ///< CPF da pessoa.
+    Data dataNascimento;  ///< Data de nascimento da pessoa.
+    Endereco enderecoPessoal;  ///< Endereço da pessoa.
+    string estadoCivil;  ///< Estado civil da pessoa.
+    int qtdFilhos;  ///< Quantidade de filhos da pessoa.
 
 public:
+
+    /**
+   * @brief Construtor padrão
+   * 
+   * @details Construtor que inicializa uma pessoa com atributos vazios.
+   */
     Pessoa() {}
+
+    /**
+     * @brief Construtor parametrizado
+     * 
+     * @details Construtor que inicializa uma pessoa com valores para cada atributo.
+     * 
+     * @param nome Nome da pessoa.
+     * @param cpf CPF da pessoa.
+     * @param dataNascimento Data de nascimento da pessoa.
+     * @param enderecoPessoal Endereço da pessoa.
+     * @param estadoCivil Estado civil da pessoa.
+     * @param qtdFilhos Quantidade de filhos da pessoa.
+     */
     Pessoa(string nome, string cpf, Data dataNascimento, Endereco enderecoPessoal,string estadoCivil, int qtdFilhos);
+
+     /**
+     * @brief Destrutor
+     * 
+     * @details Destrutor padrão da classe Pessoa.
+     */
     ~Pessoa() {}
-    // Getters e setters
+    
+    /**
+     * @brief Getter para nome
+     * 
+     * @details Retorna o nome da pessoa. O método é const e não pode modificar atributos da classe.
+     * 
+     * @return string O nome da pessoa.
+     */
     string getNome() const;
+
+    /**
+     * @brief Setter para nome
+     * 
+     * @details Determina o nome da pessoa.
+     * 
+     * @param nome Valor a ser atribuido ao nome da pessoa.
+     */
     void setNome(string nome) ;
 
+    /**
+     * @brief Getter para CPF
+     * 
+     * @details Retorna o CPF da pessoa. O método é const e não pode modificar atributos da classe.
+     * 
+     * @return string O CPF da pessoa.
+     */
     string getCPF() const ;
+
+    /**
+     * @brief Setter para CPF
+     * 
+     * @details Determina o CPF da pessoa.
+     * 
+     * @param cpf Valor a ser atribuito ao CPF da pessoa.
+     */
     void setCPF(string cpf) ;
 
+    /**
+     * @brief Getter para dataNascimento
+     * 
+     * @details Retorna a data de nascimento da pessoa. O método é const e não pode modificar atributos da classe.
+     * 
+     * @return Data A data de nascimento da pessoa.
+     */
     Data getDataNascimento() const ;
+
+    /**
+     * @brief Setter para dataNascimento
+     * 
+     * @details Determina a data de nascimento da pessoa.
+     * 
+     * @param dataNascimento A data de nascimento da pessoa.
+     */
     void setDataNascimento(Data dataNascimento) ;
 
+
+    /**
+     * @brief Getter para enderecoPessoal
+     * 
+     * @details Retorna o endereço pessoal da pessoa. O método é const e não pode modificar atributos da classe.
+     * 
+     * @return Endereco Um tipo Endereço que representa o endereço pessoal da pessoa.
+     */
     Endereco getEnderecoPessoal() const ;
+
+    /**
+     * @brief Setter para enderecoPessoal
+     * 
+     * @details Determina o endereço pessoal da pessoa.
+     * 
+     * @param enderecoPessoal O endereço pessoal da pessoa.
+     */
     void setEnderecoPessoal(Endereco enderecoPessoal) ;
 
+
+    /**
+     * @brief Getter para estadoCivil
+     * 
+     * @details Retorna o estado civil da pessoa. O método é const e não pode modificar atributos da classe.
+     * 
+     * @return string O estado civil da pessoa.
+     */
     string getEstadoCivil() const ;
+
+     /**
+     * @brief Setter para estadoCivil
+     * 
+     * @details Determina o estado civil da pessoa.
+     * 
+     * @param estadoCivil Estado civil da pessoa.
+     */
     void setEstadoCivil(string estadoCivil) ;
     
+    /**
+     * @brief Getter para qtdFilhos
+     * 
+     * @details Retorna a quantidade de filhos da pessoa. O método é const e não pode modificar atributos da classe.
+     * 
+     * @return int a quantidade de filhos da pessoa.
+     */
     int getQtdFilhos() const ;
+
+    /**
+     * @brief Setter para qtdFilhos
+     * 
+     * @details Determina a quantidade de filhos da pessoa.
+     * 
+     * @param qtdFilhos Quantidade de filhos da pessoa.
+     */
     void setQtdFilhos(int qtdFilhos) ;
 };
 
